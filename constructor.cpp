40be212ec4121a343014f9a664cbf977d4b44532
maru-tirth <tirthmaru14@gmail.com>
2025-03-24 09:07:10 +0530

    Edited the codes

--- constructor.cpp
+++ constructor.cpp
@@ -1,57 +1,56 @@
 #include <iostream>
 using namespace std;
 // COMMON USE OF CONSTRUCTORS
-class Demp
-{
-    string name;
-    int age;
 
-public:
-    // Default constructor
-    Demp()
-    {
-        name = "John";
-        age = 25;
-    }
-    // Parameterized constructor
-    Demp(string n, int a)
-    {
-        name = n;
-        age = a;
-    }
-    // Copy constructor
-    Demp(Demp &d)
-    {
-        name = d.name;
-        age = d.age;
-    }
-    void display()
-    {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-    }
+// class Demo
+// {
+//     string name;
+//     int age;
+// public:
+//     // Default constructor
+//     Demo()
+//     {
+//         name = "John";
+//         age = 25;
+//     }
+//     // Parameterized constructor
+//     Demo(string n, int a)
+//     {
+//         name = n;
+//         age = a;
+//     }
+//     // Copy constructor
+//     Demo(Demo &a)
+//     {
+//         name = a.name;
+//         age = a.age;
+//     }
+//     void display()
+//     {
+//         cout << "Name: " << name << endl;
+//         cout << "Age: " << age << endl;
+//     }
+//     ~Demo()
+//     {
+//         cout << "\nDestructor called" << endl;
+//     }
+// };
 
-    ~Demp()
-    {
-        cout << "Destructor called" << endl;
-    }
-};
+// int main()
+// {
+//     Demo d1;
+//     Demo d2("Robert", 30);
+//     Demo d3 = d2;
+//     cout << "Object 1: " << endl;
+//     d1.display();
+//     cout << "\nObject 2: " << endl;
+//     d2.display();
+//     cout << "\nObject 3: " << endl;
+//     d3.display();
+//     return 0;
+// }
 
-int main()
-{
-    Demp d1;
-    Demp d2("Robert", 30);
-    Demp d3 = d2;
-    cout << "Object 1" << endl;
-    d1.display();
-    cout << "Object 2" << endl;
-    d2.display();
-    cout << "Object 3" << endl;
-    d3.display();
-    return 0;
-}
-
-// ADDITION OF TWO NUMBERS
+// ADDITION OF TWO INTEGERS
 
 // class Addition
 // {
@@ -87,7 +86,9 @@ int main()
 // {
 //     int real, img;
 // public:
+//     // Default constructor
 //     Complex() {}
+//     // Parameterized constructor
 //     Complex(int a, int b)
 //     {
 //         real = a;
@@ -97,6 +98,7 @@ int main()
 //     {
 //         cout << real << " + " << img << "i" << endl;
 //     }
+//     // Function to add two complex numbers
 //     Complex sum(Complex C1)
 //     {
 //         Complex C3;
@@ -108,12 +110,12 @@ int main()
 
 // int main()
 // {
-//     Complex obj1(3, 4);
-//     obj1.showData();
-//     Complex obj2(5, 6);
-//     obj2.showData();
-//     Complex obj3;
-//     obj3 = obj1.sum(obj2);
-//     obj3.showData();
+//     Complex C1(3, 4);
+//     C1.showData();
+//     Complex C2(5, 6);
+//     C2.showData();
+//     Complex C3;
+//     C3 = C1.sum(C2);
+//     C3.showData();
 //     return 0;
 // }
